58aa582ae1ee58b12a48fc59dbc532e0831b4393
Darius Vozbutas <darius.vozbutas@nfq.lt>
2017-12-04 15:51:56 +0200

    updating java8 to debian stretch

--- java8/build/setup_docker.sh
+++ java8/build/setup_docker.sh
@@ -3,12 +3,10 @@
 set -x
 set -e
 
-# Setup apt
-echo "deb http://ftp.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get update
 
 # Java 8
-apt-get install -y --no-install-recommends -t jessie-backports ca-certificates-java openjdk-8-jre-headless 
+apt-get install -y --no-install-recommends ca-certificates-java openjdk-8-jre-headless
 
 source /usr/local/build_scripts/cleanup_apt.sh
 
