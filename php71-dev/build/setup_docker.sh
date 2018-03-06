aa00d74349c15e230c0439f87a6abb17990fa292
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2018-03-06 13:28:49 +0200

    Upgrade phpunit package from 5.4.x to 6.5.x on php-dev images >= 7.0

--- php71-dev/build/setup_docker.sh
+++ php71-dev/build/setup_docker.sh
@@ -85,7 +85,7 @@ phpDisableModule zip
 
 
 # install phpunit
-apt-get install -y --force-yes --no-install-recommends phpunit54
+apt-get install -y --force-yes --no-install-recommends phpunit65
 phpenmod phar
 
 
