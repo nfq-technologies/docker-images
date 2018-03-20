7f573c88f1042cca749792d872535e5b4abc59ca
Darius Vozbutas <darius.vozbutas@nfq.lt>
2018-03-20 13:50:34 +0200

    updating docker to 17.12

--- docker17ce/build/setup_docker.sh
+++ docker17ce/build/setup_docker.sh
@@ -11,7 +11,7 @@ apt-get install -y apt-transport-https ca-certificates
 echo 'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
 curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-get update
-apt-get install -y docker-ce=17.03.0~ce-0~debian-stretch
+apt-get install -y docker-ce=17.12.1~ce-0~debian
 
 
 cp -frv /build/files/* / || true
