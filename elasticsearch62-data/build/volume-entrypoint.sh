b16e40716638fded8db3c2adf9e434143bd70c3c
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2018-05-11 11:35:19 +0300

    Added elasticsearch62 image

---
+++ elasticsearch62-data/build/volume-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+/entrypoint.sh &
+esPid=$!
+
+source /build/helpers/common.sh
+
+wait_for_connection "localhost:9200" 15
+wget "localhost:9200/" -O -
+
+# set default shards and replicas
+curl -XPUT -d '{"template":"*","settings":{"number_of_shards":2,"number_of_replicas":0}}' http://localhost:9200/_template/default
+
+
+kill $esPid
+wait
+
+tar cf /volume/backup.tar /var/elasticsearch/data
+sync
+
