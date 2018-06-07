b1adfbd7eb2b279b1f83481fc1f79d731cdcfc86
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2018-06-07 14:57:15 +0300

    Added lib-smartclient version 12.0p

---
+++ lib-smartclient120/build/setup_docker.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -x
+set -e
+
+URL="http://www.smartclient.com/builds/SmartClient/12.0p/LGPL"
+DATE=$(wget -O - $URL | grep 'LGPL/20..\-..\-.."' | sed 's/^.*href="[^"]*\(20..\-..\-..\)".*$/\1/'| head -n1)
+URL="$URL/$DATE/SmartClient_v120p_${DATE}_LGPL.zip"
+
+
+DDIR=/home/project/lib/smartclient
+cd /tmp
+wget -O sc.zip $URL
+unzip sc.zip
+rm -fr sc.zip
+mkdir -p $DDIR/smartclientSDK/isomorphic
+mv SmartClient_*/smartclientSDK/isomorphic $DDIR/smartclientSDK/
+rm -fr /tmp	SmartSlient_*
+chmod -R -x+X,o-w $DDIR
+chown -R 1000:1000 $DDIR
+
+
+cp -frv /build/files/* /
+
+#source /usr/local/build_scripts/cleanup_apt.sh
+
