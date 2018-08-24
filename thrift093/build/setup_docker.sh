e9459e54eb09878cfcbc82ef6c986b17bf8befaf
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2018-08-24 10:50:07 +0300

    Added testing container for thrift 0.9.3

---
+++ thrift093/build/setup_docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -x
+set -e
+
+
+apt-get update
+
+# Installing build tools
+apt-get install -y --no-install-recommends cmake g++ bison flex make
+
+# Downloat thrift source
+cd /tmp
+wget https://www.apache.org/dist/thrift/0.9.3/thrift-0.9.3.tar.gz
+tar -xzf thrift-0.9.3.tar.gz
+
+# Build thrift compiler
+cd thrift-0.9.3/build/cmake
+cmake ../../
+make
+make install
+
+# Clean up build tools
+apt-get purge -y cmake g++ bison flex make
+apt-get autoremove -y
+
+# Copy runtime files
+cp -frv /build/files/* / || true
+
+# Clean up APT when done.
+source /usr/local/build_scripts/cleanup_apt.sh
+
