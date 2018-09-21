45abc431167bf65c9bb0e49a187bece082cdbb3b
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2018-09-21 10:52:54 +0300

    Added node10 image

---
+++ node10/build/setup_docker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+set -x
+set -e
+
+# Install fontforge
+apt-get update
+apt-get install -y --no-install-recommends rlwrap python
+
+# Manually download and install node deb
+file="$(wget -qO - https://deb.nodesource.com/node_10.x/pool/main/n/nodejs/ | sed 's/href="\([^"]*\)">/\n\1\n/g' | grep -i '^nodejs_[0-9\.\-]*nodesource1_amd64\.deb$' | tail -n1)"
+wget -qO nodejs.deb https://deb.nodesource.com/node_10.x/pool/main/n/nodejs/$file
+dpkg -i nodejs.deb
+rm nodejs.deb
+
+# yarn
+npm install -g yarn
+
+# grunt
+npm install -g grunt-cli
+
+# sass
+npm install -g node-sass --unsafe
+
+# gulp
+npm install -g gulp-cli
+
+# bower
+npm install -g bower
+
+# uglify js
+npm install -g uglify-js
+
+# uglifycss
+npm install -g uglifycss
+
+# typescript
+npm install -g typescript
+
+# typescript linter
+npm install -g tslint
+
+# Copy runtime files
+cp -frv /build/files/* / || true
+
+# Clean up APT when done.
+source /usr/local/build_scripts/cleanup_apt.sh
+
