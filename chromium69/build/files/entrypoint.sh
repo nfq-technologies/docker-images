db4c7012e0ff002456d8ee41207eb23e297fcc34
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2018-10-31 16:18:20 +0200

    Disable shared memory use for chromium

--- chromium69/build/files/entrypoint.sh
+++ chromium69/build/files/entrypoint.sh
@@ -27,6 +27,7 @@ chromium \
 	--safebrowsing-disable-auto-update \
 	--user-data-dir=/tmp \
 	--disable-gpu \
+	--disable-dev-shm-usage \
 	--remote-debugging-port=9222 \
 	--remote-debugging-address=0.0.0.0
 
