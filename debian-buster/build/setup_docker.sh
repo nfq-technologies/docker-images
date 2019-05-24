30e0ef02802ac9230e5f31ad835b4d7e8082ac4b
Darius Vozbutas <darius.vozbutas@nfq.lt>
2019-05-24 14:40:09 +0300

    adding debian-buster image

---
+++ debian-buster/build/setup_docker.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -x
+set -e
+
+echo force-unsafe-io > /etc/dpkg/dpkg.cfg.d/02apt-speedup
+
+apt-get update
+apt-get upgrade -y
+
+
+# install standard tools
+apt-get install -y --no-install-recommends \
+	bash-completion \
+	bzip2 dnsutils \
+	less \
+	netcat-traditional \
+	patch \
+	telnet \
+	time \
+	traceroute \
+	w3m \
+	curl \
+	wget \
+	whiptail \
+	whois \
+	procps \
+	net-tools \
+
+
+# install various helper tools
+apt-get install -y --no-install-recommends \
+	vim \
+	nethogs \
+	pv \
+	git \
+	rsync \
+	iotop \
+	sysstat \
+	ngrep \
+	mc \
+	ca-certificates \
+	sudo \
+	locales \
+	man \
+	apt-transport-https \
+	gnupg \
+	unzip \
+	strace \
+	jq \
+	shellcheck \
+	gnupg \
+	dirmngr \
+
+
+# install acl to support advanced file permissions
+apt-get install -y --no-install-recommends acl
+
+# install ssh client
+apt-get install -y --no-install-recommends openssh-client
+
+# configure user project
+useradd -d /home/project -m -s /bin/bash -u 1000 -U project
+echo 'project:project' | chpasswd
+echo 'project ALL=NOPASSWD: ALL' >> /etc/sudoers
+
+# add debian-repo.nfq.lt key
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 5791B856FE7BB48D
+
+apt-get install -y --no-install-recommends cowsay
+ln -s /usr/games/cowsay /usr/local/bin/
+
+# Generate locales
+echo en_US.UTF-8 UTF-8 > /etc/locale.gen
+dpkg-reconfigure locales
+echo LC_ALL=en_US.UTF-8 > /etc/default/locale
+
+
+# vim plugins
+cd /tmp
+wget -qO- https://github.com/w0rp/ale/archive/v1.7.0.tar.gz | tar xz
+mkdir -p /home/project/.vim/pack/git-plugins/start
+mv ale-1.7.0 /home/project/.vim/pack/git-plugins/start/ale
+chown -R project:project /home/project/.vim
+
+# Fix vim mouse integration
+sed -ri 's/^([ ]*set mouse=a)/"\1/' /usr/share/vim/vim81/defaults.vim
+
+cp -frv /build/files/* /
+
+
+source /usr/local/build_scripts/cleanup_apt.sh
+
