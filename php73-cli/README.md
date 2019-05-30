e1da765c5be0c89c0c1b8d508f6bb5398353b56b
Darius Vozbutas <darius.vozbutas@nfq.lt>
2019-05-30 14:02:42 +0300

    adding php73-cli image based on debian-buster

---
+++ php73-cli/README.md
@@ -0,0 +1,125 @@
+
+
+## supported env vars
+
+
+### NFQ_PROJECT_ROOT (optional, required by some other options)
+
+This var defines path to project root (repository).
+
+#### Required by
+
+* xdebug php module
+
+
+### NFQ_ENABLE_PHP_MODULES (optional)
+
+Provide space separated lists of modules in a single quoted string
+
+example:
+    docker run -it -e NFQ_ENABLE_PHP_MODULES='pdo pdo_mysql xdebug' <this_image>
+
+#### list of available modules
+
+* amqp
+* apcu_bc
+* apcu
+* ast
+* bcmath
+* bz2
+* calendar
+* ctype
+* curl
+* dba
+* dom
+* ds
+* enchant
+* excimer
+* exif
+* fileinfo
+* ftp
+* gd
+* gearman
+* geoip
+* geos
+* gettext
+* gmp
+* gnupg
+* horde_lz4
+* http
+* ice
+* iconv
+* igbinary
+* imagick
+* imap
+* interbase
+* intl
+* json
+* ldap
+* lua
+* luasandbox
+* mailparse
+* mapi
+* mbstring
+* memcached
+* memcache
+* mongodb
+* msgpack
+* mysqli
+* mysqlnd
+* oauth
+* odbc
+* opcache
+* pdo_dblib
+* pdo_firebird
+* pdo
+* pdo_mysql
+* pdo_odbc
+* pdo_pgsql
+* pdo_sqlite
+* pgsql
+* phar
+* php-remctl
+* pinba
+* posix
+* propro
+* ps
+* pspell
+* radius
+* raphf
+* readline
+* recode
+* redis
+* rrd
+* sass
+* shmop
+* simplexml
+* smbclient
+* snmp
+* soap
+* sockets
+* solr
+* sqlite3
+* ssh2
+* stomp
+* sysvmsg
+* sysvsem
+* sysvshm
+* tideways
+* tidy
+* tokenizer
+* uploadprogress
+* uuid
+* wddx
+* wikidiff2
+* xdebug
+* xml
+* xmlreader
+* xmlrpc
+* xmlwriter
+* xsl
+* yaml
+* zip
+* zmq
+
+
---
+++ php73-cli/build/setup_docker.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+set -x
+set -e
+
+apt-get update
+
+apt-get install -y --no-install-recommends php7.3-cli \
+	libow-php7 \
+	php7.3-bcmath \
+	php7.3-bz2 \
+	php7.3-common \
+	php7.3-curl \
+	php7.3-dba \
+	php7.3-enchant \
+	php7.3-gd \
+	php7.3-gmp \
+	php7.3-imap \
+	php7.3-interbase \
+	php7.3-intl \
+	php7.3-json \
+	php7.3-ldap \
+	php7.3-mbstring \
+	php7.3-mysql \
+	php7.3-odbc       odbc-mdbtools \
+	php7.3-opcache \
+	php7.3-pgsql \
+	php7.3-pspell \
+	php7.3-readline \
+	php7.3-recode \
+	php7.3-snmp       snmp \
+	php7.3-soap \
+	php7.3-sqlite3 \
+	php7.3-sybase \
+	php7.3-tidy \
+	php7.3-xml \
+	php7.3-xmlrpc \
+	php7.3-zip \
+	php-amqp \
+	php-apcu \
+	php-apcu-bc \
+	php-ast \
+	php-ds \
+	php-excimer \
+	php-gearman \
+	php-geoip \
+	php-geos \
+	php-gnupg \
+	php-horde-lz4 \
+	php-http \
+	php-igbinary \
+	php-imagick       libmagickcore-6.q16-6-extra \
+	php-lua \
+	php-luasandbox \
+	php-mailparse \
+	php-mapi \
+	php-memcache \
+	php-memcached \
+	php-mongodb \
+	php-msgpack \
+	php-oauth \
+	php-pinba \
+	php-propro \
+	php-ps \
+	php-radius \
+	php-raphf \
+	php-redis \
+	php-remctl \
+	php-rrd \
+	php-sass \
+	php-smbclient \
+	php-solr \
+	php-ssh2 \
+	php-stomp \
+	php-tideways \
+	php-uploadprogress \
+	php-uuid \
+	php-wikidiff2 \
+	php-xdebug \
+	php-yaml \
+	php-zeroc-ice \
+	php-zmq \
+
+
+#	php-gmagick \ provides more stable api but conflicts with imagick
+#	php-yac \ conflicts with php-apcu
+
+#	php-guestfs \ depends on kernel module management stuff ~50mb
+#	php-facedetect \ depends on libopencv and friends ~150mb
+#	php-libvirt-php \ depends on virtualization management
+
+
+
+# add missing odbc symlink
+#mkdir -p /usr/lib/x86_64-linux-gnu/odbc
+#ln -s /usr/lib/x86_64-linux-gnu/libodbccr.so.1 /usr/lib/x86_64-linux-gnu/odbc/libodbccr.so
+
+
+# disable all php modules
+ls -1 /etc/php/7.3/mods-available/ | sed 's/\.ini$//g' | xargs -I{} -n1 phpdismod -v ALL -s ALL {} 2>/dev/null
+
+# cleanup older versions
+rm -rf /etc/php/7.{0,1,2}
+
+# backup original php.ini
+mv /etc/php/7.3/cli/php.ini{,_orig}
+
+
+# install custom php modules
+apt-get install -y --no-install-recommends \
+    phyaml \
+#    nfq-php-tideways \ skipping for now as there is no compiled php7.3 modules
+
+
+# install dma (dragonfly mailer simple relay)
+debconf-set-selections <<< "dma dma/mailname string"
+debconf-set-selections <<< "dma dma/relayhost string mail"
+apt-get install -y --no-install-recommends dma
+echo '*: @' > /etc/aliases # force local mails to smarthost
+
+
+
+cp -frv /build/files/* / || true
+
+
+# Clean up APT when done.
+source /usr/local/build_scripts/cleanup_apt.sh
+
