79b2946efed337a727495aa4dbf3fd7c22addcc4
Tomas Leščinskas <tomas.lescinskas@nfq.lt>
2019-07-24 16:18:58 +0300

    Updated apache24-fastcgi entrypoint script for proper fastcgi host check

--- apache24-fastcgi/build/files/entrypoint.sh
+++ apache24-fastcgi/build/files/entrypoint.sh
@@ -2,17 +2,14 @@
 
 set -e
 
-
 run-parts -v /etc/rc.d
 
+# For tests when container does not have a link to fastcgi
 
-# fake fastcgi for tests when container does not have a link to fastcgi
-if [[ "x$APACHE_DOCUMENTROOT" == "x/var/www" || "x$NFQ_DOCUMENT_ROOT" == "x/var/www" ]]
-then
-	fgrep -q fastcgi /etc/hosts || echo '127.0.0.1 fastcgi' >> /etc/hosts
+if ! fgrep -q fastcgi /etc/hosts && ! host fastcgi; then
+	echo "No fastcgi host found, injecting localhost"
+	echo "127.0.0.1 fastcgi" >> /etc/hosts
 fi
 
-
 exec apache2 -DFOREGROUND
-
 
