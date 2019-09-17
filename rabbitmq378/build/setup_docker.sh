247fb7b031a09a0f2c67f9964500573b821fdc74
Darius Vozbutas <darius.vozbutas@nfq.lt>
2019-09-17 15:09:19 +0300

    adding buster based rabbitmq378 imagewq

---
+++ rabbitmq378/build/setup_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+set -e
+
+apt-get update
+
+apt-get install -y --no-install-recommends rabbitmq-server
+
+function install_plugin() {
+		local plugin="$1"
+		local URL="https://dl.bintray.com/rabbitmq/community-plugins/3.7.x/${plugin}/"
+		local file="$(wget -O - $URL 2> /dev/null | grep "${plugin}.*\.zip" | sed  's/^.*href="\([^"]*\.zip\).*$/\1/' | head -n1)"
+
+		cd tmp
+		wget -O plugin.zip "$URL/$file"
+		unzip plugin.zip
+		rm -rf plugin.zip
+		mv ${plugin}* /usr/lib/rabbitmq/lib/rabbitmq_server-3.7.8/plugins/
+		rm -f ${plugin}*
+}
+
+install_plugin rabbitmq_delayed_message_exchange
+
+cp -frv /build/files/* / || true
+
+source /usr/local/build_scripts/cleanup_apt.sh
+
