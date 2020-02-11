1772cf57d45411b2540d882aa2598d8117b78bb9
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2020-02-11 11:43:13 +0200

    Use composer phar (updatable) instead on one from debian repo

--- php73-dev/build/setup_docker.sh
+++ php73-dev/build/setup_docker.sh
@@ -68,7 +68,12 @@ phpEnableModule phar
 phpEnableModule zip
 phpEnableModule iconv
 phpEnableModule mbstring
-apt-get install -y --no-install-recommends composer
+
+curl -sSL 'https://getcomposer.org/download/1.8.4/composer.phar' > /usr/local/bin/composer.phar
+chmod a+x /usr/local/bin/composer.phar
+ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
+composer self-update
+
 
 # installl hiroku/prestissimo
 phpEnableModule curl
