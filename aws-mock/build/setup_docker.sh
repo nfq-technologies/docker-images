355ea28526010105214b3192b078d36b9de4900a
Darius Vozbutas <darius.vozbutas@nfq.lt>
2020-05-18 16:06:26 +0300

    moving aws-mock to buster

--- aws-mock/build/setup_docker.sh
+++ aws-mock/build/setup_docker.sh
@@ -7,10 +7,9 @@ set -e
 apt-get update
 
 # install pip
-apt-get install -y --no-install-recommends python3-pip
+apt-get install -y --no-install-recommends python3-pip python3-wheel python3-setuptools
 
 # install moto and flask
-pip3 install setuptools
 pip3 install moto
 pip3 install flask
 
