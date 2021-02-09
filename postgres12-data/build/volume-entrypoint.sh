c247d293189ea02143547cf9a14ef72c6031a262
Tomas Leščinskas <tomas.lescinskas@nfq.lt>
2021-02-09 15:16:09 +0200

    Image relations updated for postgres

--- postgres12-data/build/volume-entrypoint.sh
+++ postgres12-data/build/volume-entrypoint.sh
@@ -8,7 +8,6 @@ while ! PGPASSWORD=postgres psql -h localhost -U postgres -c "\c project"; do
 	sleep 1
 done
 
-#psql -h localhost -U postgres -c "CREATE DATABASE project;"
 psql -h localhost -U postgres -c "CREATE USER project WITH PASSWORD 'project' CREATEDB;"
 psql -h localhost -U postgres -c "GRANT ALL ON DATABASE project TO project;"
 
