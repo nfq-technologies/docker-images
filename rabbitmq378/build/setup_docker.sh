1cac0381406bda9c8799639ce24a1313f84390a9
Tomas Leščinskas <tomas.lescinskas@nfq.lt>
2021-06-15 20:03:16 +0300

    php74-fpm timings losen, rabbitmq378 plugin installation change

--- rabbitmq378/build/setup_docker.sh
+++ rabbitmq378/build/setup_docker.sh
@@ -9,7 +9,7 @@ apt-get install -y --no-install-recommends rabbitmq-server
 
 plugin_url="https://github.com/rabbitmq/rabbitmq-delayed-message-exchange/releases/download/v3.8.0/rabbitmq_delayed_message_exchange-3.8.0.ez"
 save_path="/usr/lib/rabbitmq/lib/rabbitmq_server-3.7.8/plugins/rabbitmq_delayed_message_exchange-3.8.0.ez"
-wget -O "$plugin_url" "$save_path"
+wget -O "$save_path" "$plugin_url"
 
 
 
