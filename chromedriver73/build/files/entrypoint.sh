1b469fd7879d8e06d6ec20a91d5c0514d59475ba
Darius Vozbutas <darius.vozbutas@nfq.lt>
2021-10-06 09:31:17 +0300

    chromedriver could start multiple times without removing lock

--- chromedriver73/build/files/entrypoint.sh
+++ chromedriver73/build/files/entrypoint.sh
@@ -12,6 +12,5 @@ init_wait_for_a_not_empty_dir
 init_wait_for_a_file
 init_wait_for_a_not_empty_file
 
-
-xvfb-run chromedriver --url-base="wd/hub" --verbose --whitelisted-ips="" --port=4444
+xvfb-run -ae /dev/stderr chromedriver --url-base="wd/hub" --verbose --whitelisted-ips="" --port=4444
 
--- chromedriver83/build/files/entrypoint.sh
+++ chromedriver83/build/files/entrypoint.sh
@@ -13,5 +13,5 @@ init_wait_for_a_file
 init_wait_for_a_not_empty_file
 
 
-xvfb-run chromedriver --url-base="wd/hub" --verbose --whitelisted-ips="" --port=4444
+xvfb-run -ae /dev/stderr chromedriver --url-base="wd/hub" --verbose --whitelisted-ips="" --port=4444
 
