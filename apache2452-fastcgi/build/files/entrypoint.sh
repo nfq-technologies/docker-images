4c918ca585c9e8a868974f76008ef67446553cd9
Marius Tamulevičius <marius.tamulevicius@nfq.lt>
2022-02-28 13:30:03 +0200

    apache2452-fastcgi image was created

---
+++ apache2452-fastcgi/build/files/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+run-parts -v /etc/rc.d
+
+# For tests when container does not have a link to fastcgi
+
+if ! fgrep -q fastcgi /etc/hosts && ! host fastcgi; then
+	echo "No fastcgi host found, injecting localhost"
+	echo "127.0.0.1 fastcgi" >> /etc/hosts
+fi
+
+exec apache2 -DFOREGROUND
+
