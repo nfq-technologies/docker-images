891e8be37c0efe10034872597933bd4c277130ae
Tomas Leščinskas <tomas.lescinskas@nfq.lt>
2022-06-06 15:26:40 +0300

    Apache2452 adaptation for gitlab

--- apache2452-fastcgi/build/files/entrypoint.sh
+++ apache2452-fastcgi/build/files/entrypoint.sh
@@ -6,7 +6,7 @@ run-parts -v /etc/rc.d
 
 # For tests when container does not have a link to fastcgi
 
-if ! fgrep -q fastcgi /etc/hosts && ! host fastcgi; then
+if [ ! -f "/builds/" ] && ! fgrep -q fastcgi /etc/hosts && ! host fastcgi; then
 	echo "No fastcgi host found, injecting localhost"
 	echo "127.0.0.1 fastcgi" >> /etc/hosts
 fi
