1962004131b1b3c88c1285adb3678b374c3f909b
simonas.radzius <simonas.radzius@nfq.lt>
2022-06-06 14:40:33 +0300

    Changed Dockerfile to use aws-tools as base image

--- aws-docker-tools/build/setup_docker.sh
+++ aws-docker-tools/build/setup_docker.sh
@@ -6,26 +6,11 @@ set -e
 # Update apt repos
 apt-get update
 
-# Install python and docker dependencies
-apt-get install --no-install-recommends -y python3-pip python3-setuptools docker.io
-
-# Install installer dependencies
-python3 -m pip install -U pip
-
-# The actual install
-pip install awsebcli
-
-# Install awscli v2
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-./aws/install
-rm -rf awscliv2.zip ./aws
+# Install docker dependencies
+apt-get install --no-install-recommends -y docker.io
 
 # Copy runtime files
 cp -frv /build/files/* / || true
-
-# Chown project home to project
-chown -R project:project /home/project
 
 # Clean up APT when done.
 source /usr/local/build_scripts/cleanup_apt.sh
