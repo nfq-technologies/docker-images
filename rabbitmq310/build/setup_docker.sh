527685e92a9eabee8c680b4593e742291a1d5d67
Tomas Leščinskas <tomas.lescinskas@nfq.lt>
2022-07-19 11:00:58 +0300

    Rabbitmq 3.8 and 3.10 images created

---
+++ rabbitmq310/build/setup_docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+apt-get update
+
+wget https://packagecloud.io/rabbitmq/rabbitmq-server/packages/debian/bullseye/rabbitmq-server_3.10.6-1_all.deb/download.deb
+
+apt -y install erlang-base erlang-crypto erlang-eldap erlang-inets erlang-mnesia erlang-os-mon erlang-parsetools erlang-public-key erlang-runtime-tools erlang-ssl erlang-syntax-tools erlang-tools erlang-xmerl ./download.deb
+
+#apt -y install ./download.deb
+rm ./download.deb
+
+cp -frv /build/files/* / || true
+
+source /usr/local/build_scripts/cleanup_apt.sh
+
