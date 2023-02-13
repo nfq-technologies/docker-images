3f31689dbe7fe4ba968985b6ea0b253ce88c8fbe
Vilius Dudenas <vilius.dudenas@nfq.lt>
2023-02-13 10:01:14 +0200

    added wait

--- apache24/build/files/entrypoint.sh
+++ apache24/build/files/entrypoint.sh
@@ -4,6 +4,15 @@ set -e
 
 run-parts -v /etc/rc.d
 
+source /tools/functions_init.sh
+
+# default features
+init_use_startup_trigger
+init_wait_for_a_dir
+init_wait_for_a_not_empty_dir
+init_wait_for_a_file
+init_wait_for_a_not_empty_file
+
 # For tests when container does not have a link to fastcgi
 
 if [ ! -d "/builds/" ] && ! fgrep -q fastcgi /etc/hosts && ! host fastcgi; then
