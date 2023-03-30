bc7ff42d5b3921059dfb63800c568d4167f58800
Tomas Leščinskas <tomas.lescinskas@nfq.lt>
2023-03-30 15:05:31 +0300

    Fixed hosts file addition of new entries for registries

--- _tools/generate_gitlab_pipelines.sh
+++ _tools/generate_gitlab_pipelines.sh
@@ -34,7 +34,7 @@ ${image}:
   stage: $level
   before_script:
     # this ip has to go to host
-    - echo "docker.nfq.lt 172.18.1.71" >> /etc/hosts
+    - echo "172.18.1.171 docker.nfq.lt" >> /etc/hosts
     - docker login -u \$dockerhub_user -p \$dockerhub_token
     - docker login -u \$nfqhub_user -p \$nfqhub_token https://docker.nfq.lt
   script: 'cd $image && make all-amd64 && make push-manifest && make publish'
@@ -45,7 +45,7 @@ ${image}_arm64:
   stage: $level
   before_script:
     # this ip has to go to host
-    - echo "docker.nfq.lt 172.30.20.101" >> /etc/hosts
+    - echo "172.30.20.101 docker.nfq.lt" >> /etc/hosts
     - docker login -u \$dockerhub_user -p \$dockerhub_token
     - docker login -u \$nfqhub_user -p \$nfqhub_token https://docker.nfq.lt
   script: 'cd $image && make all-arm64'
