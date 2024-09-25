745d405ec18cf4569101b8b07bde142a11b8f8b8
Tomas Lescinskas <tomas.lescinskas@nfq.lt>
2024-09-25 16:13:52 +0300

    Graph gen update, removed php script

--- _tools/graph-gen.sh
+++ _tools/graph-gen.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-# Directory containing Dockerfiles
-DOCKERFILE_DIR="./"
 OUTPUT_FILE="_docs/media/image_relations.gv"
-echo "digraph DockerDependencies {" > $OUTPUT_FILE
-echo "  label=\"Image relations\";" >> $OUTPUT_FILE
-echo "  graph [fontname=sans];" >> $OUTPUT_FILE
-echo "  rankdir=\"LR\";" >> $OUTPUT_FILE
-echo "  node [style=filled shape=box fontname=sans fillcolor=white];" >> $OUTPUT_FILE
+
+{
+    echo "digraph DockerDependencies {"
+    echo "  label=\"Image relations\";"
+    echo "  graph [fontname=sans pad="0" nodesep="0.1" ranksep="0.2"];"
+    echo "  rankdir=\"LR\";"
+    echo "  node [style=filled shape=box fontname=sans fillcolor=white];"
+} > $OUTPUT_FILE
 
 # Function to extract the FROM line (parent image)
 extract_parent_image() {
@@ -24,15 +25,15 @@ for dockerfile in $DOCKERFILES; do
         parent_image=$(extract_parent_image "$dockerfile")
 
         # If a parent image is found, create a Graphviz edge
-        if [[ ! -z "$parent_image" ]]; then
-            echo "\"$image_name\" [fillcolor=orange label=\"$image_name\"];" >> $OUTPUT_FILE
-            echo  "\"$parent_image\" -> \"$image_name\";" >> $OUTPUT_FILE
+        if [[ -n "$parent_image" ]]; then
+            echo "\"$image_name\" [fillcolor=orange label=\"$image_name\"];" >>$OUTPUT_FILE
+            echo "\"$parent_image\" -> \"$image_name\";" >>$OUTPUT_FILE
         fi
     fi
 done
 
 # End the Graphviz DOT file
-echo "}" >> $OUTPUT_FILE
+echo "}" >>$OUTPUT_FILE
 
 # Optional: Generate the dependency graph using Graphviz (install graphviz package first)
 dot -Tpng $OUTPUT_FILE -o _docs/media/image_relations.png
