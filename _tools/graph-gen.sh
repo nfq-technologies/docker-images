49543f96f4f314bb765846030fd9c1da93e92353
Antanas Dubovikas <antanas.dubovikas@nfq.com>
2024-09-25 15:37:26 +0300

    removed graph-gen.sh

--- _tools/graph-gen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker run -it --platform linux/amd64 --rm \
-                -v ${PWD}:/home/project/images/nfqlt:rw \
-                nfqlt/php74-cli \
-                bash -c '\
-                    phpenmod gv ;\
-                    /home/project/images/nfqlt/_tools/graph-gen.php' ;
