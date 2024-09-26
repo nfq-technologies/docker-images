4a40d373605a2d544072bb9a0e9e39ed05c4041d
Antanas Dubovikas <antanas.dubovikas@vinted.com>
2024-09-26 08:40:05 +0300

    Added node21 & nodeserver21 images (#266)
    * Added node21 & nodeserver21 images
    
    * README.md update
    
    * removed graph-gen.sh
    
    ---------
    
    Co-authored-by: Antanas Dubovikas <antanas.dubovikas@nfq.com>

---
+++ node21/build/setup_docker.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -x
+set -e
+
+arch="$([ "`uname -m`" = "aarch64" ] && echo "arm64" || echo "amd64")"
+
+# install node
+ver=21
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$ver.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
+apt-get update && apt-get install -t nodistro nodejs -y
+
+# yarn
+npm install -g yarn
+
+# grunt
+npm install -g grunt-cli
+
+# sass
+npm install -g sass --unsafe
+
+# gulp
+npm install -g gulp-cli
+
+# bower
+npm install -g bower
+
+# uglify js
+npm install -g uglify-js
+
+# uglifycss
+npm install -g uglifycss
+
+# typescript
+npm install -g typescript
+
+# typescript linter
+npm install -g tslint
+
+# typescript executor
+npm install -g ts-node
+
+# Copy runtime files
+cp -frv /build/files/* / || true
+
+# Clean up APT when done.
+source /usr/local/build_scripts/cleanup_apt.sh
+
