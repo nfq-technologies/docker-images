e9b7e1ca21710e1959f5c19568b472224f33cd16
Antanas Dubovikas <antanas.dubovikas@nfq.com>
2025-01-30 13:47:36 +0200

    Removed cp from build script

--- cloud-tools/build/setup_docker.sh
+++ cloud-tools/build/setup_docker.sh
@@ -34,21 +34,12 @@ unzip awscliv2.zip
 ./aws/install
 rm -rf awscliv2.zip ./aws
 
-# Copy runtime files
-cp -frv /build/files/* / || true
-
 # Install azure cli
 curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-
 
 # Chown project home to project
 chown -R project:project /home/project
 
-
-# Copy runtime files
-cp -frv /build/files/* / || true
-
 # Clean up APT when done.
 source /usr/local/build_scripts/cleanup_apt.sh
-
 
