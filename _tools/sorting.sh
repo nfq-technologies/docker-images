f2e9dd11c5f990eaee933a94f2002c3523672d2e
Antanas Dubovikas <antanas.dubovikas@nfq.com>
2026-02-26 14:48:36 +0200

    feat: deprecate EOL images

--- _tools/sorting.sh
+++ _tools/sorting.sh
@@ -11,8 +11,8 @@ function parse_children() {
 	local parents="$1"
 
 	for parent in ${!parents}; do
-		# Print to stdout the output
-		grep "FROM $parent\$" ./*/Dockerfile | cut -d"/" -f2 | xargs -I {} echo "nfqlt/"{}
+		# Print to stdout the output, sorted and excluding deprecated
+		grep "FROM $parent\$" ./*/Dockerfile | grep -v './_deprecated/' | sort | cut -d"/" -f2 | xargs -I {} echo "nfqlt/"{}
 	done
 }
 
@@ -22,14 +22,15 @@ level_1=""
 
 for dockerfile in ${root_dockerfiles}; do
 	image="$(echo "${dockerfile}" | /usr/bin/cut -d'/' -f2)"
-	level_1="nfqlt/$image
-$level_1"
+	level_1="$level_1
+nfqlt/$image"
 done
+level_1="$(echo "$level_1" | sed '/^$/d' | sort)"
 
-level_2="$(parse_children level_1)"
-level_3="$(parse_children level_2)"
-level_4="$(parse_children level_3)"
-level_5="$(parse_children level_4)"
+level_2="$(parse_children level_1 | sort)"
+level_3="$(parse_children level_2 | sort)"
+level_4="$(parse_children level_3 | sort)"
+level_5="$(parse_children level_4 | sort)"
 
 
 if [[ $1 =~ [1-5]{1} ]]; then
